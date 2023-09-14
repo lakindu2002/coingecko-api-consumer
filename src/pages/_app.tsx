5be0b3209aa14186b19cc3e38be53a0be84020bf
lakindu hewawasam <lakindu2002@live.com>
2023-09-14 20:32:09 +0530

    deployment: adding amplify hosting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import 'coingecko/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "coingecko/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
