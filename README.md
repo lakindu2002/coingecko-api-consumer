006f316d1be0e133543d82035429dc8a8278a975
lakindu hewawasam <lakindu2002@live.com>
2023-09-15 18:58:29 +0530

    deployment: removing amplify deployment

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 This project consists of a Next.js project that consumes the Coingecko API
 
 ## Deployment
-
-The app is deployed via Amplify Hosting. 
-Deployment URL - https://dev.dthznngy0edw2.amplifyapp.com
--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
   rewrites: async () => ([
     {
       source: '/api/:slug*',
